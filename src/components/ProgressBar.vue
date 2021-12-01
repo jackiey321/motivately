cf3f1e59aed13e652bc5fcef50db398bb7e0ac13
Jaclyn Yang <jackiey321@gmail.com>
2021-12-01 02:18:51 -0500

    created progress bar component

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h3>Progress: {{ Math.round(finished / all * 100) }}%</h3>
+    <div id="bar">
+      <div id="progress">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProgressBar',
+  props: {
+    finished: Number,
+    all: Number
+  }
+};
+</script>
+
+<style scoped>
+  h3 {
+    font-size: 24px;
+  }
+
+  #bar {
+    width: 100%;
+    height: 10px;
+    background-color: #EAE6D8;
+    border-radius: 10px;
+  }
+
+  #progress {
+    width: 42%;
+    height: 10px;
+    background-color: #E3B28F;
+    border-radius: 10px;
+  }
+</style>
