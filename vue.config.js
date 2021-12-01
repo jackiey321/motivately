e44422c10cb9a8d835a5292fd76928fa55a0d67e
Jaclyn Yang <jackiey321@gmail.com>
2021-12-01 02:16:46 -0500

    mpa setup

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  pages: {
+    'index': {
+      entry: './src/pages/Home/main.js',
+      template: 'public/index.html',
+      title: 'Home',
+      chunks: [ 'chunk-vendors', 'chunk-common', 'index' ]
+    },
+    'kanban': {
+      entry: './src/pages/Kanban/main.js',
+      template: 'public/index.html',
+      title: 'Kanban',
+      chunks: [ 'chunk-vendors', 'chunk-common', 'kanban' ]
+    }
+  }
+}
