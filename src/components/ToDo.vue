d855cfd0dc2eb0da806ef0a2c59899bbb6a6794c
Jaclyn Yang <jackiey321@gmail.com>
2021-12-01 02:19:20 -0500

    Created to do components

---
+++ src/components/ToDo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input id="todo" type="checkbox">
+    <label for="todo" class="ml-2"><h3>{{ name }}</h3></label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDo',
+  props: {
+    name: String
+  }
+};
+</script>
+
+<style scoped>
+  div {
+    display: flex;
+  }
+
+  h3 {
+    font-size: 24px;
+  }
+</style>
