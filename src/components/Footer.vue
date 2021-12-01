4ded6ef6c2d56131a0b57e718d7df83b08c84586
Jaclyn Yang <jackiey321@gmail.com>
2021-12-01 02:19:29 -0500

    created footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="p-2">
+    <p class="m-0">Copyright &copy; 2021 motivately</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+};
+</script>
+
+<style scoped>
+  div {
+    color: #805C3B;
+    text-align: center;
+    background-color: #BDD7C1;
+  }
+</style>
