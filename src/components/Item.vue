c7690e726f77e3e4d0643903c4aac8582325cbb2
Jaclyn Yang <jackiey321@gmail.com>
2021-12-02 17:40:08 -0500

    update label color of task items based on type

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="text-left item row m-2 p-3">
-    <div id="label" class="col-2">
+    <div v-if="type === 'personal'" class="personal-label">
+    </div>
+    <div v-else-if="type === 'work'" class="work-label">
+    </div>
+    <div v-else-if="type === 'other'" class="other-label">
+    </div>
+    <div v-else class="label">
     </div>
     <div class="col">
-      <h3>{{ name }}</h3>
+      <h3 class="mb-0">{{ name }}</h3>
+      <hr class="m-0 mb-1">
       <p class="m-0">{{ date }}</p>
     </div>
   </div>
@@ -14,7 +21,8 @@ export default {
   name: 'Item',
   props: {
     name: String,
-    date: String
+    date: String,
+    type: String
   }
 };
 </script>
@@ -24,13 +32,28 @@ export default {
     font-size: 20px !important;
   }
 
-  #label {
+  .personal-label {
+    background-color: #daadcb;
+  }
+
+  .work-label {
+    background-color: #809dd1;
+  }
+
+  .other-label {
+    background-color: #ebd29e;
+  }
+
+  .label {
     background-color: #D0D0D0;
+  }
+
+  .personal-label, .work-label, .other-label, .label {
     border-radius: 10px;
+    width: 5%;
   }
 
   .item {
-    height: 86px;
     background-color: white;
     border-radius: 10px;
   }
