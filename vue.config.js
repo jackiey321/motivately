29b411bc3327221e67be065d47ade2c1498955a1
Jaclyn Yang <jackiey321@gmail.com>
2021-12-03 18:54:19 -0500

    change public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/motivately/docs/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/motivately" : "/",
   pages: {
     'index': {
       entry: './src/pages/Home/main.js',
